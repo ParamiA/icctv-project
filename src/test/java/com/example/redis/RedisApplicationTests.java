b3ae7145c4c1685d84d8e96bd4f342272d028ed6
Parami Abeysekara <Parami.A@CreativeSoftware.com>
2020-03-10 10:54:16 +0530

    First commit

---
+++ src/test/java/com/example/redis/RedisApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class RedisApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplicationTests.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redis/Video.java
@@ -0,0 +1,10 @@
+package com.example.redis;
+
+public class Video {
+
+    private String id;
+    private String url;
+    private String status;
+
+
+}
